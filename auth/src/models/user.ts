fa1426a291a9cf51ed8ac6550826a6281837090f
akasataikisiti <nama0you0chu@gmail.com>
2023-06-11 05:09:38 +0900

    ユーザモデルの実際の型をMongooseの定義を拡張して

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -9,8 +9,15 @@ interface UserAttrs {
 
 // An interface that describes the properties
 // that a User Model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
