3bd45e0376afe1a033540112b7a866492cb8b647
akasataikisiti <nama0you0chu@gmail.com>
2023-06-13 03:01:33 +0900

    jwtトークンを作る処理作成

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-errors";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import jwt from "jsonwebtoken";
+
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
 
@@ -35,6 +36,18 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      "asdf"
+    );
+
+    // Store it on session object
+    req.session = { jwt: userJwt };
 
     res.status(201).send(user);
   }
